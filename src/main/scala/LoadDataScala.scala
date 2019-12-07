9bf370683cbfba0654dfb9a8a0ff539812d13658
MPerezMateo <m.perez.mateo@hotmail.com>
2019-12-07 13:33:57 +0100

    Missing values removal

--- src/main/scala/LoadDataScala.scala
+++ src/main/scala/LoadDataScala.scala
@@ -1,6 +1,6 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{sum, col}
-import org.apache.spark.sql.functions.{isnan, when, count, col}
+import org.apache.spark.sql.functions.{sum, isnan, when, count, col}
+
 
 object LoadDataScala {
 
@@ -24,17 +24,20 @@ object LoadDataScala {
       .drop("NASDelay")
       .drop("SecurityDelay")
       .drop("LateAircraftDelay")
-
+      .drop("CancellationCode")
     // We use this to see NA values.
-    // second_DF.select(second_DF.columns.map(c => sum(col(c).isNull.cast("int")).alias(c)): _*).show
-
-    val third_DF = second_DF.drop("CancellationCode")
 
     // TODO: Look for missing values.
-    third_DF.select([count(when(isnan(c), c)).alias(c) for c in df.columns]).show()
+    second_DF.select(second_DF.columns.map(c => sum(col(c).isNull.cast("int")).alias(c)): _*).show()
+    printf("Number of rows before removal: " + second_DF.count()) // 7453215
+    var third_DF = second_DF.na.drop();
+    // third_DF.select(second_DF.columns.map(c => sum(col(c).isNull.cast("int")).alias(c)): _*).show() //this is unnecesary
+    printf("Number of rows after removal: " + third_DF.count()) // 7453193
+
+    // We can see that there are only 22 missing values in TailNum and 0 in the rest of the variables
 
     // TODO: Check Correlations between variables
-
-    println(initial_DF.printSchema())
-  }
+    //initial_DF.printSchema()
+    //third_DF.show(100) //This shows you the dataframe in a table like format an rows u ask it
+   }
 }
